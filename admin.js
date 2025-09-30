5ba440c15b5dd4764c9c565a3e9f76bc7fb481b9
nibolau <dombrowskinicolasmat@gmail.com>
2025-09-30 14:05:12 -0300

    Delete admin.js

--- admin.js
+++
@@ -1,8 +0,0 @@
-//arquivo para a edição de itens do catálogo etc
-
-//importação da conexão da database
-const conn = require("./db");
-
-conn.query(
-    'INSERT INTO `catalogo` (name, description imgSrc, infillPreset, colorPreset)  VALUES () '
-  )
